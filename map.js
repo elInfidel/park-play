76681db7ddde274b83294141465b506bb3649e8a
rmit-s3372913-Liam-Parker <s3372913@student.rmit.edu.au>
2017-07-29 15:23:22 +1000

    Initiating map
    Added leaflet map and basic initiation code.

---
+++ map.js
@@ -0,0 +1,25 @@
+
+var map = L.map('mapid').setView([-37.717, 144.836], 13);
+
+	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+		maxZoom: 18,
+		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+		id: 'mapbox.streets'
+	}).addTo(map);
+
+	L.marker([-37.717, 144.836]).addTo(map);
+
+	//L.circle([51.508, -0.11], {
+	//	color: 'red',
+	//	fillColor: '#f03',
+	//	fillOpacity: 0.5,
+	//	radius: 500
+	//}).addTo(map);
+
+	//L.polygon([
+	//	[51.509, -0.08],
+	//	[51.503, -0.06],
+	//	[51.51, -0.047]
+	//]).addTo(map);
