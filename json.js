93d6ecc478fd7515fba1d92d724c248f1382279c
Marcy <marcelax2918@gmail.com>
2017-07-29 15:00:28 +1000

    Jquery reads the suburbs of each park using the brimbank park data

--- json.js
+++ json.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
-  alert('YAY');
+
+  $.getJSON( "http://data.gov.au/geoserver/playground/wfs?request=GetFeature&typeName=ckan_e8d3580c_3981_47ab_a675_573805c3fa86&outputFormat=json", function( data ) {
+    var items = [];
+    $.each( data.features, function( key, val ) {
+      items.push( "<li id='" + val.properties + "'>" + val.properties.suburb + "</li>" );
+  });
+
+  $( "<ul/>", {
+    "class": "my-new-list",
+    html: items.join( "" )
+  }).appendTo( "body" );
+});
+
+
 });
