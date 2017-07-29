39852fba96f99f9ad438d19fce78f9e200b2c781
Daniel Mason <me@danielmason.co.nz>
2017-07-29 13:09:29 +1000

    Added menu sidebar

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,11 @@
 
 
-function myFunction(x) {
-    x.classList.toggle("change");
+function toggleMenu(ele) {
+    ele.classList.toggle("menu-button-active");
+    
+    if($(".menu-button-active").html()) {
+      $("#menu").animate({left: '0px'});
+    } else {
+      $("#menu").animate({left: '-20%'});
+    }
 }
