40f4b7977021196cbfbd44a38a5dfe873663b586
Liam Parker <parker.liam5@gmail.com>
2017-07-29 16:11:48 +1000

    Testing Json data view

--- json.js
+++ json.js
@@ -1,6 +1,11 @@
 var mapObjects = new Array();
 $(document).ready(function(){
 
-  $.getJSON( "http://data.gov.au/geoserver/playground/wfs?request=GetFeature&typeName=ckan_e8d3580c_3981_47ab_a675_573805c3fa86&outputFormat=json", function( data ) {
-    mapObjects = data.features.properties;
+  $.getJSON( "http://data.gov.au/geoserver/playground/wfs?request=GetFeature&typeName=ckan_e8d3580c_3981_47ab_a675_573805c3fa86&outputFormat=json",
+  function( data ) 
+  { 
+    mapObjects = data.features;
+    LoadMapData(mapObjects);
+  });
+
 });
--- map.js
+++ map.js
@@ -1,15 +1,36 @@
 
-var map = L.map('mapid').setView([-37.717, 144.836], 13);
+var map;
 
-	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-		maxZoom: 18,
-		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-			'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-		id: 'mapbox.streets'
-	}).addTo(map);
+Init();
 
-	L.marker([-37.717, 144.836]).addTo(map);
+function Init()
+{
+    map = L.map('mapid').setView([-37.717, 144.836], 13);
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+	    maxZoom: 18,
+	    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+		    '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+		    'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+	    id: 'mapbox.streets'
+    }).addTo(map);
+}
+
+function LoadMapData(features)
+{
+    for (var i = 0; i < features.length; i++) 
+    {
+        L.marker(
+            [features[i].properties.lat, features[i].properties.long],
+            false,
+            true,
+            features[i].properties.site_name
+        ).addTo(map);
+    }
+}
+
+
+
+
 
 	//L.circle([51.508, -0.11], {
 	//	color: 'red',
